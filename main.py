d2815489723a49f06c27c075d1b2a827ca4be5c6
Bani <himelighoshal@gmail.com>
2025-06-29 01:21:19 +0530

    🚫 No refills so fast! Serving 429s with charm

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from fastapi.responses import JSONResponse
 from routers import register
 from routers import view_users
 
@@ -16,7 +17,15 @@ app.include_router(view_users.router)
 
 app.state.limiter = limiter
 
+
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+@app.exception_handler(RateLimitExceeded)
+async def custom_rate_limit_handler(request, exc):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "☕ Whoa, slow down! You're sipping too fast. Try again in a minute."}
+    )
 
 # Inject shared store into both routers
 register.users = users
